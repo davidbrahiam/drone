435175cfb6d4b8fedf1e258c7213a685ef9e25cc
davidbrahiam <dbrahiam@gmail.com>
2019-06-23 19:24:17 -0300

    Corrigindo Workspace

--- tum_simulator_ws/build/ros_ardrone/ardrone_autonomy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/davidbrahiam/tum_simulator_ws/src/ros_ardrone/ardrone_autonomy
-# Build directory: /home/davidbrahiam/tum_simulator_ws/build/ros_ardrone/ardrone_autonomy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
